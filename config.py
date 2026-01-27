6282a8ab4f9695efe443c8fd9cebd146c43e8826
Yash Samrat <yashsamrat10080@gmail.com>
2026-01-27 20:23:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-# Bot Configuration
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
@@ -8,12 +7,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", 0))
 MONGO_URL = os.environ.get("MONGO_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "startlove")
 
-# Sudo Users (comma separated)
-SUDO_USERS = [int(x) for x in os.environ.get("SUDO_USERS", "").split(",") if x.strip().isdigit()]
-
-# Buttons
-REPORT_BUTTON = "🚨 Send Report"
-ADD_SUDO_BUTTON = "➕ Add Sudo"
-REMOVE_SUDO_BUTTON = "➖ Remove Sudo"
-STATS_BUTTON = "📊 Stats"
-BACK_BUTTON = "🔙 Back"
+SUDO_USERS = []
+sudo_str = os.environ.get("SUDO_USERS", "")
+if sudo_str:
+    SUDO_USERS = [int(x.strip()) for x in sudo_str.split(",") if x.strip().isdigit()]
