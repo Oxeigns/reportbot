bc10f3cee215a347995b63f5fef8ff283274c530
Yash Samrat <yashsamrat10080@gmail.com>
2026-01-27 20:30:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH")
 OWNER_ID = int(os.environ.get("OWNER_ID", 0))
-MONGO_URL = os.environ.get("MONGO_URL", "")
+MONGO_URL = os.environ.get("MONGO_URL")
 DB_NAME = os.environ.get("DB_NAME", "startlove")
 
 SUDO_USERS = []
-sudo_str = os.environ.get("SUDO_USERS", "")
-if sudo_str:
+if sudo_str := os.environ.get("SUDO_USERS"):
     SUDO_USERS = [int(x.strip()) for x in sudo_str.split(",") if x.strip().isdigit()]
